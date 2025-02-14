fb9ca6bc542907a7dfbcb926c22fee01bd407a8f
0xJAMES <liiijz@live.com>
2025-02-14 10:15:00 +0800

    Improve JSON-RPC type definitions

--- alchemy/types.go
+++ alchemy/types.go
@@ -1,5 +1,6 @@
 package alchemy
 
+// JsonRpcRequest represents a JSON-RPC 2.0 request
 type JsonRpcRequest struct {
 	JsonRpc string `json:"jsonrpc"`
 	Id      int    `json:"id"`
@@ -7,12 +8,27 @@ type JsonRpcRequest struct {
 	Params  []any  `json:"params"`
 }
 
+// JsonRpcResponse represents a JSON-RPC 2.0 response
 type JsonRpcResponse struct {
 	JsonRpc string        `json:"jsonrpc"`
-	Id      int           `json:"id"` 
-	Result  any           `json:"result"`
+	Id      int           `json:"id"`
+	Result  any           `json:"result,omitempty"`
 	Error   *JsonRpcError `json:"error,omitempty"`
 }
 
+// JsonRpcError represents a JSON-RPC 2.0 error
 type JsonRpcError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+// NewJsonRpcRequest creates a new JSON-RPC request
+func NewJsonRpcRequest(id int, method string, params ...any) *JsonRpcRequest {
+	return &JsonRpcRequest{
+		JsonRpc: "2.0",
+		Id:      id,
+		Method:  method,
+		Params:  params,
+	}
 }
